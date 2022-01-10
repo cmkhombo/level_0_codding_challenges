b9871a9e4b6f9ea8c6d892d7e665a867101aa53d
cmkhombo <cmkhombo@gmail.com>
2022-01-10 15:20:59 +0200

    coming on Task08

--- Task08.js
+++ Task08.js
@@ -20,13 +20,14 @@ function numbersToTime(number) {
     str1 = 'Hour';
   }
 
-  if (min > 1) {
+  if (min > 1 || min === 0) {
     str2 = 'Minutes';
   } else {
     str2 = 'Minute';
   }
   return hours + ' ' + str1 + ',' + min + ' ' + str2;
 }
-console.log(numbersToTime(60));
-console.log(numbersToTime(133));
+console.log(numbersToTime(120));
+console.log(numbersToTime(500));
 console.log(numbersToTime(65));
+console.log(numbersToTime(61));
